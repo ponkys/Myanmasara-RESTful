f94a647aeda48ea1124d9c595cd5f676e4721430
“ponkys” <diego.mosquera.soto@gmail.comssh-keygen -t rsa -C diego.mosquera.soto@gmail.comgit config --global color.ui truegit config --global user.name ponkysgit config --global user.email diego.mosquera.soto@gmail.com>
2016-10-13 16:23:25 +0200

    cors added in dev mode with port 3000

--- app.js
+++ app.js
@@ -7,10 +7,28 @@ var apiController = require('./controllers/apiController');
 var Post = require('./models/Post.model.js');
 var User = require('./models/User.model.js');
 var db = 'mongodb://localhost/myanmasara';
+cors = require('cors');
+
+
 
 mongoose.connect(db);
 
 var port = process.env.PORT || 4000;
+
+// cors
+var originsWhitelist = [
+  'http://localhost:3000',      //this is my front-end url for development
+];
+
+var corsOptions = {
+  origin: function(origin, callback){
+        var isWhitelisted = originsWhitelist.indexOf(origin) !== -1;
+        callback(null, isWhitelisted);
+  },
+  credentials:true
+}
+//here is the magic
+app.use(cors(corsOptions));
 
 app.use('/assets', express.static(__dirname + '/public'));
 
